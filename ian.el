b05c77efd29562a4e39428b4a2255a3575f19f71
Ian Price <ianprice90@googlemail.com>
2013-05-09 22:57:26 +0100

    Get rid of vala config

--- ian.el
+++ ian.el
@@ -299,14 +299,6 @@
 (require 'ioccur)
 
 
-;; vala
-(autoload 'vala-mode "vala-mode" "Major mode for editing Vala code." t)
-(add-to-list 'auto-mode-alist '("\\.vala$" . vala-mode))
-(add-to-list 'auto-mode-alist '("\\.vapi$" . vala-mode))
-;(add-to-list 'file-coding-system-alist ("\\.vala$" . utf-8))
-;(add-to-list 'file-coding-system-alist ("\\.vapi$" . utf-8))
-
-
 (define-skeleton ruby-class
   "Ruby class skeleton"
   "Enter the name of the class: "
