948caa6b0ced54ced79ce1534062a4f7c16c66fc
Ian Price <ianprice90@googlemail.com>
2013-05-11 02:29:12 +0100

    Get rid of BNF mode

--- ian.el
+++ ian.el
@@ -85,20 +85,6 @@
         (line-beginning-position 2)))))
 
 (require 'pabbrev)
-;; Taken from http://www.reddit.com/r/emacs/comments/af2sg/bnfmode_for_emacs/
-(define-generic-mode 'bnf-mode
- () ;; comment char: inapplicable because # must be at start of line
- nil ;; keywords
- '(
-   ("^#.*" . 'font-lock-comment-face) ;; comments at start of line
-   ("^<[^ \t\n]*?>" . 'font-lock-function-name-face) ;; LHS nonterminals
-   ("<[^ \t\n]*?>" . 'font-lock-builtin-face) ;; other nonterminals
-   ("::=" . 'font-lock-const-face) ;; "goes-to" symbol
-   ("\|" . 'font-lock-warning-face) ;; "OR" symbol
-   )
- '("\\.bnf\\'") ;; filename suffixes
- nil ;; extra function hooks
- "Major mode for BNF highlighting.")
 
 (global-set-key (kbd "\C-c +") 'hs-toggle-hiding)
 ;; I wish there was a programming "super mode" i could hook into
