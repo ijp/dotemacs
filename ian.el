d257d945a2520db4249fbf9611fd9c1bec7d5267
Ian Price <ianprice90@googlemail.com>
2013-08-27 05:59:44 +0100

    Remove binding for org-iswitchb

--- ian.el
+++ ian.el
@@ -536,7 +536,6 @@ If buffer doesn't exist, does nothing."
 ;(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
 (global-set-key "\C-cc" 'org-capture)
 
 (setq org-log-done t)
