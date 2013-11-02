9cdf14aea0c7ec8303982a11992e57c517fb4f71
Ian Price <ianprice90@googlemail.com>
2013-11-02 22:49:50 +0000

    Get rid of extra config loading stuff

--- init.el
+++ init.el
@@ -33,17 +33,6 @@
 (regen-autoloads)
 (load custom-file 'noerror)
 
-;; You can keep system- or user-specific customizations here
-(setq system-specific-config (concat user-emacs-directory system-name ".el")
-      user-specific-config (concat user-emacs-directory user-login-name ".el")
-      user-specific-dir (concat user-emacs-directory user-login-name))
-(add-to-list 'load-path user-specific-dir)
-
-(if (file-exists-p system-specific-config) (load system-specific-config))
-(if (file-exists-p user-specific-config) (load user-specific-config))
-(if (file-exists-p user-specific-dir)
-  (mapc #'load (directory-files user-specific-dir nil ".*el$")))
-
 ;;; init.el ends here
 (put 'set-goal-column 'disabled nil)
 (put 'upcase-region 'disabled nil)
