625f9f67872cf6891e9d6efcd7630af4c71b8452
Ian Price <ianprice90@googlemail.com>
2014-09-05 00:44:04 +0100

    Remove old starter-kit browser comments

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -32,16 +32,6 @@
       oddmuse-directory (concat user-emacs-directory "oddmuse")
       xterm-mouse-mode t)
 
-;; Set this to whatever browser you use
-;; (setq browse-url-browser-function 'browse-url-firefox)
-;; (setq browse-url-browser-function 'browse-default-macosx-browser)
-;; (setq browse-url-browser-function 'browse-default-windows-browser)
-;; (setq browse-url-browser-function 'browse-default-kde)
-;; (setq browse-url-browser-function 'browse-default-epiphany)
-;; (setq browse-url-browser-function 'browse-default-w3m)
-;; (setq browse-url-browser-function 'browse-url-generic
-;;       browse-url-generic-program "~/src/conkeror/conkeror")
-
 ;; Transparently open compressed files
 (auto-compression-mode t)
 
