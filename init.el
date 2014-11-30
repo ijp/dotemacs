a3afd3da2f748f43117ab0c59fe0a477da0137d5
Ian Price <ianprice90@googlemail.com>
2014-11-30 17:43:45 +0000

    Don't require cl

--- init.el
+++ init.el
@@ -13,7 +13,6 @@
 ;; These should be loaded on startup rather than autoloaded on demand
 ;; since they are likely to be used in every session
 
-(require 'cl)
 (require 'ffap)
 (require 'ansi-color)
 (require 'recentf)
