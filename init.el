ff46c5d4dfaf131dfc82436e22fb133e391c00b3
Ian Price <ianprice90@googlemail.com>
2014-11-30 17:43:05 +0000

    Get Haskell mode from package.el

--- init.el
+++ init.el
@@ -268,12 +268,9 @@
 
 
 ;;;; Haskell
-(load "~/src/emacs/haskell-mode-2.8.0/haskell-site-file.el")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 ;;;; Functions
 (defun rename-file-and-buffer (new-name)
